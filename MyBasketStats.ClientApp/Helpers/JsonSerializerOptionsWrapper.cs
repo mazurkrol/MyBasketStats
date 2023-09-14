baa3dae04bda98ca7ed845181d45806bb1e55fc8
Piotr Mazur <piotrjm02@gmail.com>
2023-09-15 01:43:15 +0200

    add: Mvc client app with imported dto models and client class prototype

---
+++ MyBasketStats.ClientApp/Helpers/JsonSerializerOptionsWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace MyBasketStats.ClientApp.Helpers
+{
+    public class JsonSerializerOptionsWrapper
+    {
+        public JsonSerializerOptions Options { get; }
+
+        public JsonSerializerOptionsWrapper()
+        {
+            Options = new JsonSerializerOptions(
+                JsonSerializerDefaults.Web);
+        }
+    }
+}
---
+++ MyBasketStats.ClientApp/MyBasketStatsAPIClient.cs
@@ -0,0 +1,107 @@
+﻿using MyBasketStats.ClientApp.Helpers;
+using MyBasketStats.ClientApp.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+namespace MyBasketStats.Client;
+
+public class MyBasketStatsAPIClient
+{
+    private HttpClient _client;
+    private readonly JsonSerializerOptionsWrapper _jsonSerializerOptionsWrapper;
+    public MyBasketStatsAPIClient(HttpClient client,
+        JsonSerializerOptionsWrapper jsonSerializerOptionsWrapper,
+        string route)
+    {
+        _client = client;
+        _jsonSerializerOptionsWrapper = jsonSerializerOptionsWrapper ??
+            throw new ArgumentNullException(nameof(jsonSerializerOptionsWrapper));
+        _client.BaseAddress = new Uri("https://localhost:7091");
+        _client.Timeout = new TimeSpan(0, 0, 30);
+    }
+
+    public async Task<string> GetResourcesAsync(string route)
+    {
+        var request = new HttpRequestMessage(
+            HttpMethod.Get,
+            route);
+        request.Headers.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+        var response = await _client.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        return content;
+    }
+
+    public async Task<string> GetResourceAsync(string route, int id)
+    {
+        var request = new HttpRequestMessage(
+                       HttpMethod.Get,
+                                  $"{route}/{id}");
+        request.Headers.Accept.Add(
+                       new MediaTypeWithQualityHeaderValue("application/json"));
+
+        var response = await _client.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        return content;
+    }
+
+    public async Task<string> DeleteResourceAsync(string route, int id)
+    {
+        var request = new HttpRequestMessage(
+                       HttpMethod.Delete,
+                                  $"{route}/{id}");
+        request.Headers.Accept.Add(
+                       new MediaTypeWithQualityHeaderValue("application/json"));
+
+        var response = await _client.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        return content;
+    }
+
+    public async Task<string> DeleteResourceAsync(string route, int id)
+    {
+        var request = new HttpRequestMessage(
+                       HttpMethod.Delete,
+                                  $"{route}/{id}");
+        request.Headers.Accept.Add(
+                       new MediaTypeWithQualityHeaderValue("application/json"));
+
+        var response = await _client.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        return content;
+    }
+
+    public async Task<string> CreateResourceAsync(string route, string serializedResourceToCreate)
+    {        
+        var request = new HttpRequestMessage(
+            HttpMethod.Post,
+            "api/teams");
+        request.Headers.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+        request.Content = new StringContent(serializedResourceToCreate);
+        request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        var response = await _client.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        return content;
+    }
+
+}
